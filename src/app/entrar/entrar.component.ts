92f4e888ed8ebbac828d868e4e050914b9a44655
annysena <annysena.as44@gmail.com>
2021-08-30 18:18:53 -0300

    Iniciando o consumo da API

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 import { AuthService } from '../service/auth.service';
 
@@ -25,11 +26,19 @@ export class EntrarComponent implements OnInit {
     this.auth.entrar(this.usuarioLogin).subscribe((resp: UsuarioLogin)=> {
       this.usuarioLogin = resp
 
-      this.usuarioLogin.foto
+      environment.token = this.usuarioLogin.token
+      environment.nome = this.usuarioLogin.nome
+      environment.foto = this.usuarioLogin.foto
+      environment.id = this.usuarioLogin.id
+
+      console.log(environment.token)
+      console.log(environment.nome)
+      console.log(environment.foto)
+      console.log(environment.id)
 
       this.router.navigate(['/inicio'])
     }, erro=> {
-      if(erro.status == 500) {
+      if(erro.status == 401) {
         alert('Usuário ou senha estão incorretos.')
       }
     })
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http'
 import { UsuarioLogin } from '../model/UsuarioLogin';
 import { Observable } from 'rxjs';
 import { Usuario } from '../model/Usuario';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,15 @@ export class AuthService {
 
   cadastrar(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>('https://blogannybe.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if(environment.token != '') {
+      ok = true
+    }
+    
+    return ok
   }
 }
