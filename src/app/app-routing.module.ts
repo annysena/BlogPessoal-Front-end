9b5594039468086b4861619682f581d76b0f9ad4
annysena <annysena.as44@gmail.com>
2021-08-31 17:23:39 -0300

    Componente tema iniciado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
 
@@ -11,7 +12,8 @@ const routes: Routes = [
 {path: 'entrar', component: EntrarComponent},
 {path: 'cadastrar', component: CadastrarComponent},
 
-{path: 'inicio', component: InicioComponent}
+{path: 'inicio', component: InicioComponent},
+{path: 'tema', component: TemaComponent}
 ];
 
 
