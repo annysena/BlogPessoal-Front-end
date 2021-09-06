36791f59ae4e314d6aec85d0809baabf6a9fe713
annysena <annysena.as44@gmail.com>
2021-09-06 17:23:23 -0300

    Implementação de PUT e DELETE de postagens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { identifierModuleUrl } from '@angular/compiler';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 
 const routes: Routes = [
 
@@ -18,7 +21,9 @@ const routes: Routes = [
 {path: 'tema', component: TemaComponent},
 
 {path: 'tema-edit/:id', component: TemaEditComponent},
-{path: 'tema-delete/:id', component: TemaDeleteComponent}
+{path: 'tema-delete/:id', component: TemaDeleteComponent},
+{path: 'postagem-edit/:id', component: PostagemEditComponent},
+{path: 'postagem-delete/:id', component: PostagemDeleteComponent}
 ];
 
 
