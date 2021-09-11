bb6f4cf906119a1ab10521d2c69d8dd35d50d9dd
annysena <annysena.as44@gmail.com>
2021-09-11 10:25:22 -0300

    Alerta postagem implementado

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { Usuario } from '../model/Usuario';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
@@ -30,7 +31,8 @@ export class InicioComponent implements OnInit {
   private router: Router,
   private temaService: TemaService,
   private postagemService: PostagemService,
-  private authService: AuthService
+  private authService: AuthService,
+  private alertas: AlertasService
   ) { }
 
   ngOnInit(){
@@ -79,7 +81,7 @@ export class InicioComponent implements OnInit {
     
     this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=> {
       this.postagem = resp
-      alert('Postagem realizada com sucesso!')
+      this.alertas.showAlertSuccess('Postagem realizada com sucesso!')
       this.postagem = new Postagem()
       this.getAllPostagens()
     })
